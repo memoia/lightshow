96d83cec38942526e7e46615ba4c9042823178ff
Ian Melnick <melnick@gmail.com>
2013-09-16 22:03:11 -0500

    Rudimentary something-or-other

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 #!env/bin/python -i
 import pyaudio
 import numpy as np
+import onsetdetection
+from scipy import signal, fftpack, stats
 from beautifulhue.api import Bridge
 from lightshow.registration import register, discover
 
@@ -16,24 +18,48 @@ resource = {'which': 'all', 'verbose': False}
 print bridge.light.get(resource)
 
 
-pa = pyaudio.PyAudio()
-pa_params = {
-    'channels': 1,
-    'format': pyaudio.paInt16,
-    'rate': 44100,
-    'frames_per_buffer': 1024,
-    'input': True,
-}
+def get_sample(seconds=2, sample_rate=44100):
+    pa = pyaudio.PyAudio()
+    pa_params = {
+        'channels': 1,
+        'format': pyaudio.paInt16,
+        'rate': sample_rate,
+        'frames_per_buffer': 1024,
+        'input': True,
+    }
+    stream = pa.open(**pa_params)
 
-stream = pa.open(**pa_params)
-data = stream.read(1024)
+    data = stream.read(sample_rate * seconds)
 
-stream.stop_stream()
-stream.close()
-pa.terminate()
+    stream.stop_stream()
+    stream.close()
+    pa.terminate()
 
-as_array = np.frombuffer(data, dtype=np.int16)
-print as_array
+    return np.frombuffer(data, dtype=np.int16)
 
-#import onsetdetection
-#print onsetdetection.detect_onsets(as_array)
+
+def prob_percentiles(buckets=3):
+    minpct = (1.0 / (buckets + 1))
+    return np.linspace(minpct, 1.0 - minpct, num=buckets)
+
+
+def refresh_time(sample_seconds, onsets):
+    return int(round(60.0 / (len(onsets) / sample_seconds)))
+
+
+refresh_seconds = 1
+
+while True:
+    sample = get_sample(refresh_seconds)
+
+    #sample = signal.resample(sample, 1024)
+    #foo = np.take(fftpack.fft(sample), signal.argrelmax(sample)[0])
+
+    onsets = onsetdetection.detect_onsets(sample)
+
+    bar = np.take(fftpack.fft(sample), onsets)
+    baz = stats.mstats.mquantiles(bar, prob=prob_percentiles())
+    print baz
+
+    refresh_seconds = refresh_time(refresh_seconds, onsets)
+    print "Refresh after %s" % refresh_seconds
